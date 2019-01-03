f0ff1c98457ff107df1fc55d7405407db35e911e
cheng <1814638551@qq.com>
2019-01-03 19:38:32 +0800

    update 20190103

--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
+import {
+  Button,
+  Select
+} from "element-ui";
+// import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+Vue.use(Button)
+Vue.use(Select)
+// Vue.use(ElementUI);
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,27 +1,43 @@
 <template>
-  <div>
-    <h3>{{ title }}</h3>
-  </div>
-
+  <el-select v-model="value" placeholder="请选择">
+    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>
+  </el-select>
 </template>
-
-
+  
  <script>
- 
-    export default {
-      data() {
-        return {
-          title: 'about111'
-        }
-      }
-    }
 
- </script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          value: "选项1",
+          label: "黄金糕"
+        },
+        {
+          value: "选项2",
+          label: "双皮奶"
+        },
+        {
+          value: "选项3",
+          label: "蚵仔煎"
+        },
+        {
+          value: "选项4",
+          label: "龙须面"
+        },
+        {
+          value: "选项5",
+          label: "北京烤鸭"
+        }
+      ],
+      value: ""
+    };
+  },
+  methods: {}
+};
+</script>
 
 
 <style scope lang='less'>
-  div h3 {
-    color: red;
-  }
-
 </style>
